a66fc9971182f58112bd68a04b5348627c6fa484
Dheeraj Gandhi <gandhidheeraj9@gmail.com>
2020-11-05 02:42:48 +0530

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
-import './App.css';
-import Approuter from "./router/Router"
-import Register from "./components/Register/Register"
-import Login from "./components/Login/Login"
-import Home from "./components/Home/Home"
-import Footer from "./components/Footer/Footer"
+import "./App.css";
+import Approuter from "./router/Router";
+import Register from "./components/Register/Register";
+import Login from "./components/Login/Login";
+import Home from "./components/Home/Home";
+import Footer from "./components/Footer/Footer";
+import Navbar from "./components/Navbar/Navbar";
 function App() {
   return (
     <div className="App">
       {/* <Register /> */}
+      <Navbar />
       {/* <Login /> */}
       {/* <Home /> */}
-      <Approuter />
+      {/* <Approuter /> */}
       {/* <Footer /> */}
     </div>
   );
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,29 @@
+import "./Navbar.css";
 function Navbar() {
-    return (
-        <div>
-            
+  return (
+    <div>
+      <nav>
+        <div class="outside_div">
+          <div class="hamburger">
+            <div class="line"></div>
+            <div class="line"></div>
+            <div class="line"></div>
+          </div>
+          <ul class="navlinks">
+            <li class="navlink" id="aboutusmain">
+              hello
+            </li>
+            <li class="navlink" id="techinjust">
+              Technologies
+            </li>
+            <li class="navlink" id="contactme">
+              Contact me
+            </li>
+          </ul>
         </div>
-    )
+      </nav>
+    </div>
+  );
 }
 
-export default Navbar
+export default Navbar;

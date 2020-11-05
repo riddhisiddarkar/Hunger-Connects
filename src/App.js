79322c244fe250f5a22ac62ddbf5c4a0d993acc3
Dheeraj Gandhi <gandhidheeraj9@gmail.com>
2020-11-05 06:14:25 +0530

    update navbar

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import "./App.css";
 import Approuter from "./router/Router";
 import Register from "./components/Register/Register";
@@ -6,15 +5,6 @@ import Login from "./components/Login/Login";
 import Home from "./components/Home/Home";
 import Footer from "./components/Footer/Footer";
 import Navbar from "./components/Navbar/Navbar";
-=======
-import './App.css';
-import Approuter from "./router/Router"
-import Register from "./components/Register/Register"
-import Login from "./components/Login/Login"
-import Home from "./components/Home/Home"
-import Footer from "./components/Footer/Footer"
-import Location from "./components/Location/Location"
->>>>>>> f69b1b5c21a6c0b3cc925151538b4cbe7f213f4e
 function App() {
   return (
     <div className="App">
@@ -23,10 +13,7 @@ function App() {
       {/* <Login /> */}
       {/* <Home /> */}
       {/* <Approuter /> */}
-<<<<<<< HEAD
-=======
-      <Location></Location>
->>>>>>> f69b1b5c21a6c0b3cc925151538b4cbe7f213f4e
+
       {/* <Footer /> */}
     </div>
   );

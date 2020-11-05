2a78d4b7eb428ee1ac45d2d5c16dd24fffbd89a1
tejasimangale <tejasimangale@gmail.com>
2020-11-05 11:48:05 +0530

    Responsive Landing page

--- src/App.js
+++ src/App.js
@@ -5,18 +5,19 @@ import Location from "./components/Location/Location"
 import Login from './components/Login/Login';
 import Register from './components/Register/Register';
 import HelpUs from './components/HelpUS/Help_us'
+import Home from './components/Home/Home'
 function App() {
   return (
     <div className="App">
       {/* <Register /> */}
       {/* <Navbar /> */}
       {/* <Login /> */}
-      {/* <Home /> */}
+      <Home />
       {/* <Approuter /> */}
       {/* <Location></Location> */}
       {/* <FoodCardBank /> */}
       {/* <Register></Register> */}
-      <Login></Login>
+      {/* <Login></Login> */}
       {/* <HelpUs/> */}
       {/* <Footer /> */}
     </div>

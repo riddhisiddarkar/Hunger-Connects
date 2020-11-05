c5eaa6719cf232a5608633bc517d8e08bd4bb998
Dheeraj Gandhi <gandhidheeraj9@gmail.com>
2020-11-05 08:09:37 +0530

    nabar fixed

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import "./App.css";
 import Approuter from "./router/Router";
 import Register from "./components/Register/Register";
@@ -6,6 +5,7 @@ import Login from "./components/Login/Login";
 import Home from "./components/Home/Home";
 import Footer from "./components/Footer/Footer";
 import Navbar from "./components/Navbar/Navbar";
+import FoodCardBank from "./components/FoodBankcard/FoodBankCard";
 function App() {
   return (
     <div className="App">
@@ -16,17 +16,8 @@ function App() {
       {/* <Approuter /> */}
 
       {/* <Footer /> */}
-=======
-import './App.css';
-import Approuter from "./router/Router"
-import FoodCardBank from "./components/FoodBankcard/FoodBankCard"
-function App() {
-  return (
-    <div className="App">
-      <Approuter />
       {/* <Location></Location> */}
       {/* <FoodCardBank /> */}
->>>>>>> 4debd511f45df3a82e7bc2649a7787ea9cfe9907
     </div>
   );
 }

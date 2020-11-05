cdc631c6dbfe98470979a213cbd30605de956a79
tejasimangale <tejasimangale@gmail.com>
2020-11-05 10:43:37 +0530

    Help us page added

--- src/components/HelpUS/Help_us.jsx
+++ src/components/HelpUS/Help_us.jsx
@@ -11,14 +11,14 @@ function Help_us() {
                     <h6>(Put approximate number)</h6>
                 </label>
                 <div className="inpLabel2">
-                <input type="checkbox" id="children" name="children" value="Child"/>
+                <input type="checkbox" className="children" name="children" value="Child"/>
                 <label for="children" className="l2"> Are there any children?</label><br></br>
                 </div>
                 <div className="inpLabel2">
-                <input type="checkbox" id="senior_citizen" name="senior_citizen" value="Old"/>
+                <input type="checkbox" className="senior_citizen" name="senior_citizen" value="Old"/>
                 <label for="senior_citizen" className="l3"> Are there any senior citizens?</label><br></br>
                 </div>
-                <button type="submit" className="send_loc">SUBMIT</button>
+                <button type="submit" className="submit_btn">SUBMIT</button>
              </form>
             </div>
         </div>
